440db6885692426776b7ea11cec4ef12cd617dba
Max Bliss <maxbliss695@gmail.com>
2026-01-10 12:12:22 -0800

    Add function to play single round

--- app.js
+++ app.js
@@ -6,11 +6,49 @@ function getComputerChoice() {
   return choiceValue[computerChoice];
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
   let humanInput = prompt("Please choose: rock, paper, or scissors.");
   return humanInput;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound() {
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
+  let result = "";
+
+  console.log(`Human choice: ${humanChoice}`);
+  console.log(`Computer choice: ${computerChoice}`);
+
+  switch (true) {
+    case humanChoice === computerChoice:
+      {
+        result = console.log(
+          `It's a tie! Neither player gets a point.\n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`
+        );
+      }
+      break;
+    case humanChoice === CHOICES.ROCK && computerChoice === CHOICES.SCISSORS:
+    case humanChoice === CHOICES.PAPER && computerChoice === CHOICES.ROCK:
+    case humanChoice === CHOICES.SCISSORS && computerChoice === CHOICES.ROCK:
+      {
+        humanScore++;
+        result = console.log(
+          `You win! ${humanChoice} beats ${computerChoice} \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`
+        );
+      }
+      break;
+    default:
+      {
+        computerScore++;
+        console.log(
+          `You lose! ${computerChoice} beats ${humanChoice} \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`
+        );
+      }
+      break;
+  }
+}
+
+playRound();
