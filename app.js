64c92bde21459e99eae8998771b6055c10ee1ea3
Max Bliss <maxbliss695@gmail.com>
2026-01-10 11:28:18 -0800

    Add function to get computer choice

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const CHOICES = { ROCK: "rock", PAPER: "paper", SCISSORS: "scissors" };
 const choiceValue = Object.values(CHOICES);
+
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * choiceValue.length);
+  return choiceValue[computerChoice];
+}
+
+console.log(getComputerChoice());
